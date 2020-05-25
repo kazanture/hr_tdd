df86f913ac4f18b88979eb3baee83f369c3e5b9d
kazanture <endemre@yahoo.com>
2020-05-25 10:32:28 +0300

    longest-palindromic-substring

--- leetcode_tdd/longest_palindromic_subs.py
+++ leetcode_tdd/longest_palindromic_subs.py
@@ -1,3 +1,7 @@
+"""
+https://leetcode.com/problems/longest-palindromic-substring/
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+"""
 class LongestPalindromicSubs:
 
     def __init__(self):
